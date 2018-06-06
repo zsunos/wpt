da25c99886fb1a85fcf5a138bb8f337761c5aa7f
Anne van Kesteren <annevk@annevk.nl>
2018-06-06 11:55:14 +0200

    move service worker into resources too

--- service-workers/redirects/opaque-redirect.window.js
+++ service-workers/redirects/opaque-redirect.window.js
@@ -1,7 +1,7 @@
 // META: script=../service-worker/resources/test-helpers.sub.js
 
 promise_test(async t => {
-  const reg = await service_worker_unregister_and_register(t, "opaque-redirect-serviceworker.js", "./");
+  const reg = await service_worker_unregister_and_register(t, "resources/opaque-redirect-serviceworker.js", "resources/");
   t.add_cleanup(async () => await reg.unregister());
   await wait_for_state(t, reg.installing, 'activated');
   const frame = await with_iframe("resources/opaque-redirect.py");
