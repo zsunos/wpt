849eec303f30342c8afb5db2ca66254612159687
Anne van Kesteren <annevk@annevk.nl>
2018-08-27 14:36:57 +0200

    use a frame

--- service-workers/redirects/fragment.window.js
+++ service-workers/redirects/fragment.window.js
@@ -4,6 +4,9 @@ promise_test(async t => {
   const reg = await service_worker_unregister_and_register(t, "resources/fragment-serviceworker.js", "resources/");
   t.add_cleanup(async () => await reg.unregister());
   await wait_for_state(t, reg.installing, 'activated');
-  const response = await fetch("resources/test");
-  assert_equals(response.url, new URL("resources/test.txt#success", location));
+  const frame = await with_iframe("resources/fragment-frame.html");
+  await new Promise((resolve, reject) => {
+    self.resolve = resolve;
+    self.reject = reject;
+  });
 }, "Forward response fragments");
--- service-workers/redirects/resources/fragment-serviceworker.js
+++ service-workers/redirects/resources/fragment-serviceworker.js
@@ -1,7 +1,7 @@
 onfetch = e => {
   if(e.request.url.endsWith("resources/test")) {
     e.respondWith(new Promise(async resolve => {
-      resolve(await fetch("resources/test.txt#success"));
+      resolve(await fetch("test.txt#success"));
     }));
   }
 }
